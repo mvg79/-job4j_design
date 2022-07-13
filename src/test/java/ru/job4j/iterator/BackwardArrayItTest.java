28fc8c3e70f6d80dd1b8ec78958103ca13d8e50d
mvg79 <mvg79@mail.ru>
2022-07-13 20:45:03 +0400

    Что такое итератор. [#4951]

---
+++ src/test/java/ru/job4j/iterator/BackwardArrayItTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.iterator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class BackwardArrayItTest {
+
+    @Test
+    public void whenMultiCallHasNextThenTrue() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertTrue(it.hasNext());
+        assertTrue(it.hasNext());
+        assertSame(3, it.next());
+    }
+
+    @Test
+    public void whenMultiCallHasNextThenNext() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertTrue(it.hasNext());
+        assertTrue(it.hasNext());
+        assertTrue(it.hasNext());
+        assertSame(3, it.next());
+
+    }
+
+    @Test
+    public void whenReadSequence() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertSame(3, it.next());
+        assertSame(2, it.next());
+        assertSame(1, it.next());
+        assertFalse(it.hasNext());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextFromEmpty() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[] {}
+        );
+        it.next();
+    }
+}
