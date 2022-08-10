eba6741ada5805a428fbb2e4f52eb1207433759e
mvg79 <mvg79@mail.ru>
2022-08-10 21:16:40 +0400

    Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -22,10 +22,31 @@ public class User {
                 + '}';
     }
 
+    /*@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }*/
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
     public static void main(String[] args) {
         Map<User, Object> map = new HashMap<>();
-        User user1 = new User("Mihail", 1, new GregorianCalendar(1979, 5, 25));
-        User user2 = new User("Mihail", 1, new GregorianCalendar(1979, 5, 25));
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.YEAR, 1979);
+        calendar.set(Calendar.MONTH, 5);
+        calendar.set(Calendar.DAY_OF_MONTH, 25);
+        calendar.set(Calendar.HOUR_OF_DAY, 6);
+        calendar.set(Calendar.MINUTE, 16);
+        calendar.set(Calendar.SECOND, 11);
+        calendar.set(Calendar.MILLISECOND, 22);
+        User user1 = new User("Mihail", 1, calendar);
+        User user2 = new User("Mihail", 1, calendar);
         map.put(user1, new Object());
         map.put(user2, new Object());
 
